32d720d05ffcac495e62f458d1ea29e44604fd3f
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-17 14:57:50 +0530

    Added assi-0.3

---
+++ 3_reverse_of_a2z.rb
@@ -0,0 +1,7 @@
+alphabets = ('a'..'z')
+alphabets_arr = alphabets.to_a
+
+rev_alphabets_arr = alphabets_arr.reverse
+# puts rev_alphabets_arr.length
+
+puts "All reversed alphabets : #{rev_alphabets_arr.join}"
