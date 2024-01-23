5e737157b167866af77862b4214dcaeaebfe4a1c
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-23 13:09:47 +0530

    understood argument errors

--- Module_5A/Basics_of_inheritance.rb
+++ Module_5A/Basics_of_inheritance.rb
@@ -1,37 +1,21 @@
-# class Animal
-#     def speak
-#         puts "Animal speaks"
-#     end
-# end
-
-# class Dog < Animal
-#     def bark
-#       puts "Dog barks"
-#     end
-# end
-
-
-# a = Animal.new
-# a.speak
-
-
-# puts "\n"
-
-# d = Dog.new
-# d.bark
-
-
-class Parent
-    def do_this(parameter)
-        "I'm doing this #{parameter} task today"
+class Animal
+    def speak
+        puts "Animal speaks"
     end
 end
 
-class Chilled < Parent
-    def do_this(parameter,arg = "Mr")
-        p "Hey there #{arg}! " + super(parameter)
+class Dog < Animal
+    def bark
+      puts "Dog barks"
     end
 end
 
-a = Chilled.new
-a.do_this("helu")
+
+a = Animal.new
+a.speak
+
+
+puts "\n"
+
+d = Dog.new
+d.bark
