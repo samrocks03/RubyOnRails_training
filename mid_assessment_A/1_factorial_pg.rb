d42f84bbb2baa82fd02e2ee59b2933510c4cf3c7
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-23 16:42:36 +0530

    pg-1-factorial_while

--- mid_assessment_A/1_factorial_pg.rb
+++ mid_assessment_A/1_factorial_pg.rb
@@ -1,14 +1,20 @@
 # Write a program to calculate the factorial of a given number.
 
-
-
-# Method 1 to calculate factorial
+# `Way-1` to calculate factorial
 def fact(n)
   if n<=1
     return 1
   end
   n * fact(n-1)
 end
-
-
 p fact(5)
+
+# `Way-2` to calculate factorial
+def factorial(n)
+  fac = 1
+  while n > 1
+    fac *= n
+    n -= 1
+  end
+  fac #return the `fac` value
+end
