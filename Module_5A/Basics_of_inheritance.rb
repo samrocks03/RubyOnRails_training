d50a9d6d127df76bc97b392b069c3a0ad06a879c
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-23 12:38:14 +0530

    playing more with super methods

--- Module_5A/Basics_of_inheritance.rb
+++ Module_5A/Basics_of_inheritance.rb
@@ -1,21 +1,37 @@
-class Animal
-    def speak
-        puts "Animal speaks"
+# class Animal
+#     def speak
+#         puts "Animal speaks"
+#     end
+# end
+
+# class Dog < Animal
+#     def bark
+#       puts "Dog barks"
+#     end
+# end
+
+
+# a = Animal.new
+# a.speak
+
+
+# puts "\n"
+
+# d = Dog.new
+# d.bark
+
+
+class Parent
+    def do_this(parameter)
+        "I'm doing this #{parameter} task today"
     end
 end
 
-class Dog < Animal
-    def bark
-      puts "Dog barks"
+class Chilled < Parent
+    def do_this(parameter,arg = "Mr")
+        p "Hey there #{arg}! " + super(parameter)
     end
 end
 
-
-a = Animal.new
-a.speak
-
-
-puts "\n"
-
-d = Dog.new
-d.bark
+a = Chilled.new
+a.do_this("helu")
