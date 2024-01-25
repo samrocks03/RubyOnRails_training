e8f1d876b23a438fa5cb99d7fb35650ecbeabb2a
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-25 19:53:56 +0530

    Add game with upgrade, update indentation

--- Module_3A/hang-man_game.rb
+++ Module_3A/hang-man_game.rb
@@ -1,93 +1,123 @@
 # Putting things into class:
 
 class Game
-    attr_reader :lives,:board,:my_secret_word
-    # -------INIT STATE------------
-    def initialize(word_List)
-        @lives = 7
-        @my_secret_word = word_List.sample
-        @board = set_up_the_board
-    end
+	attr_reader :lives, :board, :my_secret_word
+	# -------INIT STATE------------
+	def initialize(word_List)
+		@lives = 7
+		@my_secret_word = word_List.sample
+		@board = set_up_the_board
+	end
 
-    # ---------SETTING UP THE BOARD---------
-    def set_up_the_board
-        Array.new(my_secret_word.size){"_"}
-    end
+	# ---------SETTING UP THE BOARD---------
+	def set_up_the_board
+		Array.new(my_secret_word.size){"_"}
+	end
 
+	# ---------BOARD STATE----------
+	def board_state
+		board.join(" ")
+	end
 
-    # ---------BOARD STATE----------
-    def board_state
-        board.join(" ")
-    end
+	# --------WIN STATUS------------
+	def won?
+		board.join("") == my_secret_word
+	end
 
-    # --------WIN STATUS------------
-    def won?
-        board.join("") == my_secret_word
-    end
-
-    def lost?
-        lives <= 0
-    end
+	def lost?
+		lives <= 0
+	end
 
     # -------- MAKE GUESS -----------
-    def make_guess
-        print "Please guess a letter:"
-        gets.chomp #implicit return occurs here
-    end
+	def make_guess
+		print "Please guess a letter:"
+		gets.chomp #implicit return occurs here
+	end
 
-    def update_board(guess)
-        if my_secret_word.include?(guess)
-            i = 0
-            while i < my_secret_word.size
-                # if the guess was "A" -> it gives 'a'
-                if guess.downcase == my_secret_word[i].downcase                
-                    board[i] = my_secret_word[i]
-                end
-                i += 1
-            end
-        else
-            @lives -= 1
-        end
-    end
+
+	def update_board(guess)
+		if my_secret_word.include?(guess)
+			my_secret_word.each_char.with_index do |char, i|
+				board[i] = char.downcase if char.downcase == guess.downcase
+			end
+		else
+			@lives -= 1
+		end
+	end
 end
 
 
 # Inheriting from the Game class to create our HangMan Game
 class Hangman < Game
-    #local instance method which will return the value of lives
-    def initialize
-        super(["dog","elephant","lion","tiger","albatrauss"])
-    end
+	#local instance method which will return the value of lives
+	def initialize
+			super(["dog","elephant","lion","tiger","albatrauss"])
+	end
 
-    # ----------STARTING UP THE GAME--------------
-    def start
-        puts "Welcome To HANG_MAN
-        Please enter a single character at a time"
+	# ----------STARTING UP THE GAME--------------
+	def start
+		puts "Welcome To HANG_MAN
+		Please enter a single character at a time"
 
-        while @lives > 0 && !won?
-            # print the num. of lives
-            puts "\nYou have #{@lives} lives left."
+		while @lives > 0 && !won?
+			# print the num. of lives
+			puts "\nYou have #{@lives} lives left."
 
-            # print the board state 
-            puts board_state
+			# print the board state
+			puts board_state
 
-            # receive our required guess
-            guess = make_guess
+			# receive our required guess
+			guess = make_guess
 
-            # update the board with relevant value
-            update_board(guess)
-        end
+			# update the board with relevant value
+			update_board(guess)
+		end
 
-
-        if won?
-            puts "You won!!"
-        else
-            puts "You lost!!"
-            puts "Actual word was ~~#{@my_secret_word}~~"
-        end
-    end
+		if won?
+				puts "You won!!"
+		else
+				puts "You lost!!"
+				puts "Actual word was ~~#{@my_secret_word}~~"
+		end
+	end
 end
 
+class Jumble < Game
 
-game = Hangman.new
+  def initialize
+    super(["python", "javascript", "ruby", "java"])
+  end
+
+  def start
+    puts "Welcome to Jumble!"
+		scrambled_word = @my_secret_word.chars.shuffle.join("")
+		# Print jumbled version of word
+		if scrambled_word != @my_secret_word
+			scrambled_word = @my_secret_word.chars.shuffle.join("")
+		end
+
+    while @lives > 0 && !won?
+      puts "You have #{@lives} lives left."
+      puts "Unscramble this word: #{scrambled_word}"
+
+      guess = make_guess
+
+			if guess == @my_secret_word
+				puts "You solved it!"
+				break
+			elsif guess != @my_secret_word
+				@lives -= 1
+				puts "Sorry, try again."
+
+				if @lives <= 0
+					puts "You ran out of lives!"
+					puts "Secret word is : #{@my_secret_word}"
+          break
+				end
+			end
+		end
+  end
+end
+
+game = Jumble.new
 game.start
